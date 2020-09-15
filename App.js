ad382a88e2668dcc0264d7342c9c43b120a8f80f
joaopaulo <jptick@gmail.com>
2020-09-15 17:39:25 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content"/>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello GoStack</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#7159c1',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  title: {
+    color: '#fff',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+})
+
+export default App;
